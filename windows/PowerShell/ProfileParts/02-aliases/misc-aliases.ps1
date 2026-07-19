45e2357a66b845996f75b95881eaef60e71dfbfe
Abdul Hafiz Muhammad <hafizmuhammad.dev@outlook.com>
2026-07-19 08:53:21 -0400

    feat(powershell): add brave 25th word function and alias

--- windows/PowerShell/ProfileParts/02-aliases/misc-aliases.ps1
+++ windows/PowerShell/ProfileParts/02-aliases/misc-aliases.ps1
@@ -2,3 +2,4 @@ Set-Alias -Name godmode -Value Set-GodMode
 Set-Alias -Name ompupgrade -Value Update-OhMyPosh
 Set-Alias -Name cttwin -Value Invoke-ChrisTitusWinUtility
 Set-Alias -Name clearcb -Value Clear-Clipboard
+Set-Alias -Name brave25 -Value Get-Brave25thWord
