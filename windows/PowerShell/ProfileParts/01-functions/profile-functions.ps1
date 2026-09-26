b13d5baaa3b2e5294d2307160ffd10462e4cfd67
Abdul Hafiz Muhammad <hafizmuhammad.git@outlook.com>
2026-09-26 07:44:10 -0400

    feat(powershell): add function and alias to clear PSReadLine history

--- windows/PowerShell/ProfileParts/01-functions/profile-functions.ps1
+++ windows/PowerShell/ProfileParts/01-functions/profile-functions.ps1
@@ -8,3 +8,10 @@ function Invoke-ProfileReload { . $PROFILE }
 
 # Show command history
 function Get-CommandHistory { Get-Content (Get-PSReadLineOption).HistorySavePath }
+
+# Clear active session history and delete the saved history file
+function Clear-PSReadLineHistory {
+    Clear-History
+    [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+    Remove-Item (Get-PSReadLineOption).HistorySavePath -ErrorAction SilentlyContinue
+}
--- windows/PowerShell/ProfileParts/02-aliases/profile-aliases.ps1
+++ windows/PowerShell/ProfileParts/02-aliases/profile-aliases.ps1
@@ -2,3 +2,4 @@ Set-Alias -Name profpath -Value Get-ProfilePaths
 Set-Alias -Name reload -Value Invoke-ProfileReload
 Set-Alias -Name profsetup -Value Initialize-Setup
 Set-Alias -Name allhist -Value Get-CommandHistory
+Set-Alias -Name clearhist -Value Clear-PSReadLineHistory
